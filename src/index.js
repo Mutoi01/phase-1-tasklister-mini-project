7df6c5fbb986a8d2c8447decd0f454915ae87fc9
Mutoi01 <pngando9@gmail.com>
2022-06-24 06:15:07 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    addList(event.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function addList(input){
+  let li = document.createElement('li')
+	let btn = document.createElement('button')
+	btn.textContent = 'x'
+	btn.addEventListener('click', deleteList)
+	li.textContent = `${input}  `
+	document.querySelector('#list').appendChild(li)
+	li.appendChild(btn)
+
+}
+
+function deleteList(event){
+	event.target.parentNode.remove()
+  } 
